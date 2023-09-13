87a683c6f399785452daa6c490192115434d66b2
Rohan Huynh <rohan.huynh@mrjaffesclass.com>
2023-09-13 11:14:08 -0700

    Strings

---
+++ Strings.java
@@ -0,0 +1,15 @@
+
+
+public class Strings
+{
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        String string1 = "I am a string";
+        String string2 = string1;
+        String string3 = "I am also a string";
+        boolean stringsEqual = string1.equals(string3);
+        System.out.println(string1);
+        System.out.println(string2);
+        System.out.println(string3);
+    }
+}
